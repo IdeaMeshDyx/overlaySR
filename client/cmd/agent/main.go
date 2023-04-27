86bfce4c1d2f6ef0d8e98dd2bf9c010b2c0d397c
ericjuice <ericjuice@qq.com>
2023-04-27 08:18:57 -0700

    add comment for main

--- client/cmd/agent/main.go
+++ client/cmd/agent/main.go
@@ -7,7 +7,11 @@ import (
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
+// main is the entry point of the program
 func main() {
+	// create a new websocket agent
 	var hub agent.WsAgent
+
+	// send a message using the agent
 	hub.Send()
 }
